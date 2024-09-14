6d72b4d8845bedfbb4a0a837de5f525c94569ff9
luis revilla <lrev4766@gmail.com>
2024-09-14 08:32:32 -0500

    added dynamic theme

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,18 +1,18 @@
-import { Box, useTheme } from "@mui/material";
+import { Box } from "@mui/material";
 import GeographyChart from "../../components/GeographyChart";
 import Header from "../../components/Header";
-import { tokens } from "../../theme";
+import { useSelector } from "react-redux"; // Import useSelector to get the theme from Redux
 
 const Geography = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
+  const { currentTheme } = useSelector((state) => state.theme); // Get current theme from Redux
+
   return (
     <Box m="20px">
       <Header title="Geography" subtitle="Simple Geography Chart" />
 
       <Box
         height="75vh"
-        border={`1px solid ${colors.grey[100]}`}
+        border={`1px solid ${currentTheme?.grey || "#e0e0e0"}`} // Use grey color from the current theme
         borderRadius="4px"
       >
         <GeographyChart />
