c1aa425d545024242ca5cc40469405f1e21f8509
luis revilla <lrev4766@gmail.com>
2024-09-14 08:32:32 -0500

    config store for themereducer

--- src/store.js
+++ src/store.js
@@ -1,39 +1,17 @@
+// src/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
-
+import themeReducer from "./features/themeSlice";
 import authReducer from "./features/authSlice";
 import userReducer from "./features/userSlice";
-import databaseReducer from "./features/databaseQueriesSlice";
-import environmentReducer from "./features/environmentSlice";
-import externalServiceReducer from "./features/externalServiceSlice";
-import financeReducer from "./features/financeSlice";
-import imageReducer from "./features/imagesSlice";
-import mockDataReducer from "./features/mockDataSlice";
-import notificationReducer from "./features/notificationsSlice";
-import permissionReducer from "./features/permissionsSlice";
-import projectReducer from "./features/projectSlice";
-import settingsReducer from "./features/settingsSlice";
-import testingAndDebuggingReducer from "./features/testingDebuggingSlice";
-import versionControlReducer from "./features/versionControlSlice";
 import jobTrackerReducer from "./features/jobTrackingSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   user: userReducer,
-  database: databaseReducer,
-  environment: environmentReducer,
-  externalService: externalServiceReducer,
-  finance: financeReducer,
-  image: imageReducer,
-  mockData: mockDataReducer,
-  notification: notificationReducer,
-  permission: permissionReducer,
-  project: projectReducer,
-  settings: settingsReducer,
-  testingAndDebugging: testingAndDebuggingReducer,
-  versionControl: versionControlReducer,
+  theme: themeReducer, // Ensure themeReducer is included correctly
   jobTracker: jobTrackerReducer,
 });
 
@@ -41,7 +19,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["auth", "user", "settings"], // Only persist specific slices of the state
+  whitelist: ["auth", "user", "theme"], // Ensure 'theme' is persisted
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -51,7 +29,7 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: false, // Disable serializable check for Redux Persist
     }),
 });
 
