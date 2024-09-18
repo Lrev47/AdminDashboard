5de896e172fab9e54c8fd9c1c73f8dca61247e52
luis revilla <lrev4766@gmail.com>
2024-09-17 23:33:48 -0500

    comment out

--- src/theme.js
+++ src/theme.js
@@ -1,33 +1,33 @@
-import { createTheme } from "@mui/material/styles";
+// import { createTheme } from "@mui/material/styles";
 
-// Default theme configuration as a fallback
-export const theme = createTheme({
-  palette: {
-    mode: "light", // Default to light mode if no theme is provided
-    primary: {
-      main: "#1976d2", // Default primary color
-    },
-    secondary: {
-      main: "#dc004e", // Default secondary color
-    },
-    background: {
-      default: "#f5f5f5", // Default background color
-    },
-    text: {
-      primary: "#000000", // Default text color
-    },
-  },
-  typography: {
-    fontFamily: ["Roboto", "Arial", "sans-serif"].join(","),
-    fontSize: 14,
-    h1: {
-      fontSize: 30,
-    },
-    h2: {
-      fontSize: 26,
-    },
-    h3: {
-      fontSize: 22,
-    },
-  },
-});
+// // Default theme configuration as a fallback
+// export const theme = createTheme({
+//   palette: {
+//     mode: "light", // Default to light mode if no theme is provided
+//     primary: {
+//       main: "#1976d2", // Default primary color
+//     },
+//     secondary: {
+//       main: "#dc004e", // Default secondary color
+//     },
+//     background: {
+//       default: "#f5f5f5", // Default background color
+//     },
+//     text: {
+//       primary: "#000000", // Default text color
+//     },
+//   },
+//   typography: {
+//     fontFamily: ["Roboto", "Arial", "sans-serif"].join(","),
+//     fontSize: 14,
+//     h1: {
+//       fontSize: 30,
+//     },
+//     h2: {
+//       fontSize: 26,
+//     },
+//     h3: {
+//       fontSize: 22,
+//     },
+//   },
+// });
