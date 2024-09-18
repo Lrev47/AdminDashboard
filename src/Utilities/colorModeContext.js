af86e3acb5476d8972949f908f4a1c66c3b9b82d
luis revilla <lrev4766@gmail.com>
2024-09-17 23:33:48 -0500

    comment out

--- src/Utilities/colorModeContext.js
+++ src/Utilities/colorModeContext.js
@@ -1,35 +1,35 @@
-import { createContext, useContext, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setCurrentTheme } from "../features/themeSlice"; // Redux slice to manage themes
+// import { createContext, useContext, useMemo } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setCurrentTheme } from "../features/themeSlice"; // Redux slice to manage themes
 
-export const ColorModeContext = createContext();
+// export const ColorModeContext = createContext();
 
-export const ColorModeProvider = ({ children }) => {
-  const dispatch = useDispatch();
-  const { themes, currentTheme } = useSelector((state) => state.theme); // Get themes from Redux
-  const isDarkMode = currentTheme?.mode === "dark";
+// export const ColorModeProvider = ({ children }) => {
+//   const dispatch = useDispatch();
+//   const { themes, currentTheme } = useSelector((state) => state.theme); // Get themes from Redux
+//   const isDarkMode = currentTheme?.mode === "dark";
 
-  // Function to toggle between light and dark modes
-  const toggleColorMode = () => {
-    const newTheme = isDarkMode
-      ? themes.find((theme) => theme.mode === "light") // Find the light theme
-      : themes.find((theme) => theme.mode === "dark"); // Find the dark theme
+//   // Function to toggle between light and dark modes
+//   const toggleColorMode = () => {
+//     const newTheme = isDarkMode
+//       ? themes.find((theme) => theme.mode === "light") // Find the light theme
+//       : themes.find((theme) => theme.mode === "dark"); // Find the dark theme
 
-    if (newTheme) {
-      dispatch(setCurrentTheme(newTheme)); // Set the new theme in Redux
-    }
-  };
+//     if (newTheme) {
+//       dispatch(setCurrentTheme(newTheme)); // Set the new theme in Redux
+//     }
+//   };
 
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode,
-    }),
-    [themes, currentTheme]
-  );
+//   const colorMode = useMemo(
+//     () => ({
+//       toggleColorMode,
+//     }),
+//     [themes, currentTheme]
+//   );
 
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      {children}
-    </ColorModeContext.Provider>
-  );
-};
+//   return (
+//     <ColorModeContext.Provider value={colorMode}>
+//       {children}
+//     </ColorModeContext.Provider>
+//   );
+// };
