3678c527816b12124cb29a7e3a99d7f52d2b2821
luis revilla <lrev4766@gmail.com>
2024-09-25 18:56:44 -0500

    adjusted api path

--- src/api/jobTrackerApi.js
+++ src/api/jobTrackerApi.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/jobTracker";
+const API_URL = "http://localhost:5000/api/jobTracker/";
 
 // Helper function to set headers with Authorization token
 const setHeaders = (token) => {
